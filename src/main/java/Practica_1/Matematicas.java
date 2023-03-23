1f3798eb4b883d6cce294cfc0633cc60a0980fc7
Diego <diegocarrasco1013@gmail.com>
2023-03-23 19:10:36 +0100

    Clase Matematicas y metodo generarNumeroPi

---
+++ src/main/java/Practica_1/Matematicas.java
@@ -0,0 +1,15 @@
+package Practica_1;
+
+public class Matematicas {
+
+    public static double generarNumeroPi(long pasos) {
+        double aciertos = 0;
+        double areaCuadrado = 4;
+        for (int i = 0; i < pasos; i++) {
+            double x = Math.random();
+            double y = Math.random();
+            if (x * x + y * y <= 1) aciertos++;
+        }
+        return areaCuadrado * aciertos / pasos;
+    }
+}
