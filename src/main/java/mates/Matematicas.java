998dcd6ed9d57892266ee96600cba14e61ec56e1
Diego <diegocarrasco1013@gmail.com>
2023-03-23 19:19:56 +0100

    Control de excepciones en Matematicas

--- src/main/java/mates/Matematicas.java
+++ src/main/java/mates/Matematicas.java
@@ -3,13 +3,17 @@ package mates;
 public class Matematicas {
 
     public static double generarNumeroPi(long pasos) {
-        double aciertos = 0;
-        double areaCuadrado = 4;
-        for (int i = 0; i < pasos; i++) {
-            double x = Math.random();
-            double y = Math.random();
-            if (x * x + y * y <= 1) aciertos++;
+        try {
+            double aciertos = 0;
+            double areaCuadrado = 4;
+            for (int i = 0; i < pasos; i++) {
+                double x = Math.random();
+                double y = Math.random();
+                if (x * x + y * y <= 1) aciertos++;
+            }
+            return areaCuadrado * aciertos / pasos;
+        }catch (Exception e){
+            System.out.println("No has introducido un numero válido");
         }
-        return areaCuadrado * aciertos / pasos;
     }
 }
